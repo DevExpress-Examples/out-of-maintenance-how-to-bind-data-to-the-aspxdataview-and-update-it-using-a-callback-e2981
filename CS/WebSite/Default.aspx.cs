5524b155ef0ab6701c8f3d10a9acfdb9b545095c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using DevExpress.Web.ASPxEditors;
+using DevExpress.Web.ASPxDataView;
+
+public partial class _Default : System.Web.UI.Page
+{
+	protected void tbName_Init(object sender, EventArgs e) {
+		ASPxTextBox tb = sender as ASPxTextBox;
+		tb.ClientInstanceName = String.Format("tbName{0}", GetKeyValue(tb));
+	}
+	protected void tbDescription_Init(object sender, EventArgs e) {
+		ASPxTextBox tb = sender as ASPxTextBox;
+		tb.ClientInstanceName = String.Format("tbDescription{0}", GetKeyValue(tb));
+	}
+	protected void btnUpdate_Init(object sender, EventArgs e) {
+		ASPxButton btn = sender as ASPxButton;
+
+		btn.ClientSideEvents.Click = String.Format("function(s, e) {{ UpdateItem(\"{0}\", tbName{0}, tbDescription{0}) }}", GetKeyValue(btn));
+	}
+
+	private int GetKeyValue(Control sender) {
+		DataViewItemTemplateContainer container = sender.NamingContainer as DataViewItemTemplateContainer;
+
+		if (container.DataItem != null)
+			return (int)DataBinder.Eval(container.DataItem, "CategoryId");
+		return -1;
+	}
+	protected void callback_Callback(object source, DevExpress.Web.ASPxCallback.CallbackEventArgs e) {
+		e.Result = e.Parameter;
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to bind data to the ASPxDataView and update it, using a callback
+
+
+<p>This example shows how to bind data to the ASPxDataView and update it using a callback</p>
+
+<br/>
+
+
